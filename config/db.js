c0d5f31e0da03631203260afb550f62954202446
chak-eeb <chak_eeb@yahoo.com>
2019-11-24 20:53:07 -0500

    initial commit, add dependencies and setup connecion to db

---
+++ config/db.js
@@ -0,0 +1,25 @@
+// we can put all this in the server.js file but to not clutter it we seperate it
+// and use this file.
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// let's create an async function
+const connectDB = async () => {
+	// we'll use a try and catch block.
+	try {
+		// since mongoose.connect returns a promise >>>
+		await mongoose.connect(db, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true
+		});
+		console.log('mongoDB connected');
+	} catch (err) {
+		console.error(err.message);
+		// exit process using:
+		process.exit(1);
+	}
+};
+
+// at the end we need to export the function
+module.exports = connectDB;
